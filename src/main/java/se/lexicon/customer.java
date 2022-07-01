e29f9cdb3ec29e9bdc94803db141de7f0c125213
Julius <juliusbuchner@gmail.com>
2022-07-01 10:14:30 +0200

    First

---
+++ src/main/java/se/lexicon/customer.java
@@ -0,0 +1,40 @@
+package se.lexicon;
+
+public class customer {
+    private String firstName;
+    private String lastname;
+    private String email;
+    private int identifier;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(int identifier) {
+        this.identifier = identifier;
+    }
+}
